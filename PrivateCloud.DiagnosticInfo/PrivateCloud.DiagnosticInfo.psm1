639e8a10d3cd53265d0a5bce0c3bed37a0040c38
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2023-05-26 15:25:11 -0500

    Update PrivateCloud.DiagnosticInfo.psm1

--- PrivateCloud.DiagnosticInfo/PrivateCloud.DiagnosticInfo.psm1
+++ PrivateCloud.DiagnosticInfo/PrivateCloud.DiagnosticInfo.psm1
@@ -2817,7 +2817,6 @@ function Get-SddcDiagnosticInfo
 		#Added by JG
                 #try {
                     Show-Update "    Gathering Sample 1: CPU, I see you!"
-					Show-Update "$ClusterNodes"
                     #Ref: https://learn.microsoft.com/en-us/windows-server/storage/storage-spaces/performance-history-scripting#sample-1-cpu-i-see-you
                     $Output =""
                     $Output = $ClusterNodes | ForEach-Object {
@@ -2999,91 +2998,7 @@ function Get-SddcDiagnosticInfo
 		try {
                     Show-Update "    Gathering Sample 5: Make storage trendy again!"
                     #Ref: https://learn.microsoft.com/en-us/windows-server/storage/storage-spaces/performance-history-scripting#sample-6-memory-hog-you-can-run-but-you-cant-hide
-                    Function Format-Bytes {
-                        Param (
-                            $RawValue
-                        )
-                        $i = 0 ; $Labels = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-                        Do { $RawValue /= 1024 ; $i++ } While ( $RawValue -Gt 1024 )
-                        # Return
-                        [String][Math]::Round($RawValue) + " " + $Labels[$i]
-                    }
-
-                    Function Format-Trend {
-                        Param (
-                            $RawValue
-                        )
-                        If ($RawValue -Eq 0) {
-                            "0"
-                        }
-                        Else {
-                            If ($RawValue -Gt 0) {
-                                $Sign = "+"
-                            }
-                            Else {
-                                $Sign = "-"
-                            }
-                            # Return
-                            $Sign + $(Format-Bytes [Math]::Abs($RawValue)) + "/day"
-                        }
-                    }
-
-                    Function Format-Days {
-                        Param (
-                            $RawValue
-                        )
-                        [Math]::Round($RawValue)
-                    }
-
-                    $CSV = Get-Volume | Where-Object FileSystem -Like "*CSV*"
-
-                    $Output = $CSV | ForEach-Object {
-
-                        $N = 14 # Require 14 days of history
-
-                        $Data = $_ | Get-ClusterPerf -VolumeSeriesName "Volume.Size.Available" -TimeFrame "LastYear" | Sort-Object Time | Select-Object -Last $N
-
-                        If ($Data.Length -Ge $N) {
-
-                            # Last N days as (x, y) points
-                            $PointsXY = @()
-                            1..$N | ForEach-Object {
-                                $PointsXY += [PsCustomObject]@{ "X" = $_ ; "Y" = $Data[$_-1].Value }
-                            }
-
-                            # Linear (y = ax + b) least squares algorithm
-                            $MeanX = ($PointsXY | Measure-Object -Property X -Average).Average
-                            $MeanY = ($PointsXY | Measure-Object -Property Y -Average).Average
-                            $XX = $PointsXY | ForEach-Object { $_.X * $_.X }
-                            $XY = $PointsXY | ForEach-Object { $_.X * $_.Y }
-                            $SSXX = ($XX | Measure-Object -Sum).Sum - $N * $MeanX * $MeanX
-                            $SSXY = ($XY | Measure-Object -Sum).Sum - $N * $MeanX * $MeanY
-                            $A = ($SSXY / $SSXX)
-                            $B = ($MeanY - $A * $MeanX)
-                            $RawTrend = -$A # Flip to get daily increase in Used (vs decrease in Remaining)
-                            $Trend = Format-Trend $RawTrend
-
-                            If ($RawTrend -Gt 0) {
-                                $DaysToFull = Format-Days ($_.SizeRemaining / $RawTrend)
-                            }
-                            Else {
-                                $DaysToFull = "-"
-                            }
-                        }
-                        Else {
-                            $Trend = "InsufficientHistory"
-                            $DaysToFull = "-"
-                        }
-
-                        [PsCustomObject]@{
-                            "Volume"     = $_.FileSystemLabel
-                            "Size"       = Format-Bytes ($_.Size)
-                            "Used"       = Format-Bytes ($_.Size - $_.SizeRemaining)
-                            "Trend"      = $Trend
-                            "DaysToFull" = $DaysToFull
-                        }
-                    }
-                    $Output | Sort-Object ClusterNode | Export-Clixml ($Path + "trendyagain.xml")
+					Get-Volume | Where-Object FileSystem -Like "*CSV*" | %{$_ | Get-ClusterPerf -VolumeSeriesName "Volume.Size.Available" -TimeFrame "LastYear" | Sort-Object Time | Select-Object -Last 14} | Sort-Object ClusterNode | Export-Clixml ($Path + "trendyagain.xml")
                 }catch { Show-Warning("Unable to get Make storage trendy again! Data.  `nError="+$_.Exception.Message) }
                 
 		try {
