ad44871de1726feeb41fcd5538a97419b5df082d
PR-Liu <53802017+PR-Liu@users.noreply.github.com>
2024-08-27 16:24:02 -0700

    Add Get-VMHost and Get-ClusterResourceType to be collected. (#188)
    * Add VM info to per node gathering
    
    * Address PR comments

--- PrivateCloud.DiagnosticInfo/PrivateCloud.DiagnosticInfo.psm1
+++ PrivateCloud.DiagnosticInfo/PrivateCloud.DiagnosticInfo.psm1
@@ -900,7 +900,7 @@ function Invoke-CommonCommand (
 
     Invoke-Command -Session $Sessions $InitBlock
     Invoke-Command -Session $Sessions -AsJob -JobName $JobName -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList |
-        Add-Member -NotePropertyName ActiveSession -NotePropertyValue $Sessions.Id -PassThru
+    Add-Member -NotePropertyName ActiveSession -NotePropertyValue $Sessions.Id -PassThru
 }
 
 function RemoveCommonJobSession
@@ -2019,6 +2019,14 @@ function Get-SddcDiagnosticInfo
                 catch { Show-Warning("Unable to get Cluster Resource Parameters.  `nError="+$_.Exception.Message) }
             }
 
+            $JobStatic += start-job -Name ClusterResourceType {
+                try {
+                    Get-ClusterResourceType -Cluster $using:AccessNode |
+                    Export-Clixml ($using:Path + "GetClusterResourceType.XML")
+                }
+                catch { Show-Warning("Unable to get Cluster Resource Type.  `nError="+$_.Exception.Message) }
+            }
+
             $JobStatic += start-job -Name ClusterSharedVolume {
                 try {
                     Get-ClusterSharedVolume -Cluster $using:AccessNode |
@@ -2394,6 +2402,7 @@ function Get-SddcDiagnosticInfo
                             @{ C = 'Get-SmbServerConfiguration -CimSession _C_'; F = $null },
                             @{ C = 'Get-SmbServerNetworkInterface -CimSession _C_'; F = $null },
                             @{ C = 'Get-StorageFaultDomain -CimSession _A_ -Type StorageScaleUnit |? FriendlyName -eq _N_ | Get-StorageFaultDomain -CimSession _A_'; F = $null },
+                            @{ C = 'Get-VMHost -ComputerName _C_'; F = $null },
                             @{ C = 'Get-WindowsFeature -ComputerName _C_'; F = $null }
 
                     # These commands are specific to optional modules, add only if present
